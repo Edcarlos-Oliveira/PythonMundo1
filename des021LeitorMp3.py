89dad6b1da1ede634678a4b82d289ad5a14a0641
Edcarlos Oliveira <112654995+Edcarlos-Oliveira@users.noreply.github.com>
2023-06-10 23:15:15 -0300

    Update des021LeitorMp3.py
    Incluído 'pygame.mixer.init()'

--- des021LeitorMp3.py
+++ des021LeitorMp3.py
@@ -1,6 +1,7 @@
 print('='*6, 'Leitor Mp3', '='*6)
 print('')
 import pygame # Biblioteca para ler Mp3
+pygame.mixer.init()
 pygame.init() # Inicia a biblioteca
 pygame.mixer.music.load("des021.mp3") # Carrega o arquivo no formato
 pygame.mixer.music.play() # Para iniciar o arquivo
